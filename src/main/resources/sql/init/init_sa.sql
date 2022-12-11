11049a88c44563cd8e59a3b8905f607908c3f26e
Jakub Frączek <fraczekjakub1999@gmail.com>
2022-12-11 20:15:16 +0100

    sa and warehouse schemas fixes

--- src/main/resources/sql/init/init_sa.sql
+++ src/main/resources/sql/init/init_sa.sql
@@ -25,39 +25,33 @@ CREATE TABLE address
 
 CREATE TABLE client
 (
-    client_id           VARCHAR2(40) not null,
-    first_name          varchar(50)  not null,
-    last_name           varchar(50)  not null,
-    pesel               varchar(11)  not null,
-    birth_date          timestamp    not null,
-    login               varchar(50)  not null,
-    password            varchar(50)  not null,
-    email               varchar(50)  not null,
-    gender              ENUM ('MALE', 'FEMALE', 'UNKNOWN') not null,
-    phone_number        varchar(15)  not null,
-    bank_account_number varchar(22)  not null,
-    role                ENUM ( 'ADMIN', 'USER') not null,
-    address_id          VARCHAR(36)  not null,
-    created_at          timestamp    not null,
+    client_id  VARCHAR2(40) not null,
+    first_name varchar(50)  not null,
+    last_name  varchar(50)  not null,
+    birth_date timestamp    not null,
+    login      varchar(50)  not null,
+    password   varchar(50)  not null,
+    email      varchar(50)  not null,
+    gender     ENUM ('MALE', 'FEMALE', 'UNKNOWN') not null,
+    address_id VARCHAR(36)  not null,
+    created_at timestamp    not null,
     CONSTRAINT fk_client_address FOREIGN KEY (address_id) REFERENCES address (address_id)
 );
 
 CREATE TABLE item
 (
-    item_id     VARCHAR2(40) not null,
-    name        varchar(100) not null,
-    description varchar(500) not null,
-    category    ENUM ( 'DOM', 'ELEKTRONIKA', 'JEDZENIE', 'OBUWIE', 'UBRANIA', 'SAMOCHODY', 'NIERUCHOMOSCI', 'ZABAWKI', 'INNE') not null
+    item_id  VARCHAR2(40) not null,
+    name     varchar(100) not null,
+    category ENUM ( 'DOM', 'ELEKTRONIKA', 'JEDZENIE', 'OBUWIE', 'UBRANIA', 'SAMOCHODY', 'NIERUCHOMOSCI', 'ZABAWKI', 'INNE') not null
 );
 
 CREATE TABLE offer
 (
-    offer_id         VARCHAR2(40) not null,
-    item_id          VARCHAR2(40) not null,
-    seller_id        VARCHAR2(40) not null,
-    publication_date timestamp    not null,
-    shipment_type    ENUM ('KURIER', 'POCZTA', 'PACZKOMAT', 'ODBIOR_OSOBISTY', 'DPD') not null,
-    price            decimal      not null,
+    offer_id      VARCHAR2(40) not null,
+    item_id       VARCHAR2(40) not null,
+    seller_id     VARCHAR2(40) not null,
+    shipment_type ENUM ('KURIER', 'POCZTA', 'PACZKOMAT', 'ODBIOR_OSOBISTY', 'DPD') not null,
+    price         decimal      not null,
     CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES item (item_id),
     CONSTRAINT fk_client FOREIGN KEY (seller_id) REFERENCES client (client_id)
 );
@@ -89,9 +83,9 @@ CREATE TABLE purchase
 CREATE TABLE return
 (
     return_id   VARCHAR2(40) not null,
-    client_id   VARCHAR2(40) not null,
-    offer_id    VARCHAR2(40) not null,
-    purchase_id VARCHAR2(40) not null,
+    client_id   VARCHAR2(40),
+    offer_id    VARCHAR2(40),
+    purchase_id VARCHAR2(40),
     quantity    int          not null,
     date        timestamp    not null,
     CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES client (client_id),
